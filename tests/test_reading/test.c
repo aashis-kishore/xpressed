73277ceb04803c013adbada24f05175fb3f2fe1f
Aashis Kishore <aashiskishore@gmail.com>
2018-10-15 00:14:15 +0530

    Refactor test_reading tests structure

--- tests/test_reading/test.c
+++ tests/test_reading/test.c
@@ -6,41 +6,52 @@
 
 #include "../../src/reading/reading.h"
 
-void test1(char*);
-void test2(char*);
+void test1(char*, TestFlag);
+void test2(char*, TestFlag);
 
 // expression length (arbitrary)
 // #define EXP_LENGTH ((size_t)1024)
 
 int main(void) {
 
-    test1("Test 1: readDirtyExpression()");
+    test1("Test 1: readDirtyExpression()", NO_SKIP_TEST);
     // test 2 -- Must be skipped to run further tests
-    test2("Test 2: readDirtyExpression() [ pass no buffer, i.e NULL ]");
+    test2("Test 2: readDirtyExpression() [ pass no buffer, i.e NULL ]", SKIP_TEST);
     
     return 0;
 }
 
-void test1(char* testName) {
+void test1(char* testName, TestFlag flag) {
     printTestName(testName);
 
-    const int EXP_LENGTH = 1024;
-    char buffer[EXP_LENGTH];
+    if((flag & NO_SKIP_TEST) == NO_SKIP_TEST) {
 
-    int expLength = readDirtyExpression(buffer, EXP_LENGTH);
+        const int EXP_LENGTH = 1024;
+        char buffer[EXP_LENGTH];
 
-    if(expLength == (int)strlen(buffer)) {
-        puts("TEST 1 PASSED\n");
+        int expLength = readDirtyExpression(buffer, EXP_LENGTH);
+
+        if(expLength == (int)strlen(buffer)) {
+            puts("TEST 1 PASSED\n");
+        } else {
+            puts("TEST 1 FAILED\n");
+        }
+        
     } else {
-        puts("TEST 1 FAILED\n");
+        puts("SKIPPED\n");
     }
 }
 
-void test2(char* testName) {
+void test2(char* testName, TestFlag flag) {
     printTestName(testName);
 
-    const int EXP_LENGTH = 1024;
-    char* buffer = NULL;
+    if((flag & NO_SKIP_TEST) == NO_SKIP_TEST) {
 
-    readDirtyExpression(buffer, EXP_LENGTH);
+        const int EXP_LENGTH = 1024;
+        char* buffer = NULL;
+
+        readDirtyExpression(buffer, EXP_LENGTH);
+    } else {
+        puts("SKIPPED\n");
+    }
 }
