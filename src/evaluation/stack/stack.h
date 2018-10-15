380ca79acb36b30a981732a29405058e0f671416
Aashis Kishore <aashiskishore@gmail.com>
2018-10-15 10:57:06 +0530

    Declare stack interface

--- src/evaluation/stack/stack.h
+++ src/evaluation/stack/stack.h
@@ -1,6 +1,20 @@
 #ifndef _STACK_H
 #define _STACK_H
 
+typedef struct stack* Stack;
 
+Stack createStack(void);
+
+void destroyStack(Stack);
+
+int stackLength(Stack);
+
+void pushStack(Stack, char);
+
+int emptyStack(Stack);
+
+char popStack(Stack);
+
+char peekStack(Stack);
 
 #endif
