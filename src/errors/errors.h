842bc4415715fae17a28a9b306430902bf81f0ae
Aashis Kishore <aashiskishore@gmail.com>
2018-10-15 17:40:20 +0530

    Fix errorcode value

--- src/errors/errors.h
+++ src/errors/errors.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 typedef enum errorcode {
-    STACK_UNDERFLOW_ERROR,
+    STACK_UNDERFLOW_ERROR = -1,
     READ_ERROR = 1,
     INVALID_BUFFER_ERROR,
     INVALID_EXPRESSION_ERROR,
